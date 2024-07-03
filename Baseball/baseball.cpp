3fbbcaedd0e270598fb213c25674cb806e733f51
joenggyun.yu <jeonggyun@gmail.com>
2024-07-03 15:12:06 +0900

    Page 42

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -5,16 +5,31 @@ using namespace std;
 
 class Baseball {
 public:
-	void guess(const string& guessNumber)
+	bool isDuplicatedNumber(string guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2];
+	}
+
+	void assertIllegalArgument(const std::string& guessNumber)
 	{
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
 
 		for (auto ch : guessNumber) {
-			if (ch < '0' || ch > '9') {
-				throw invalid_argument("Must be number.");
-			}
+			if (ch < '0' || ch > '9') continue;
+			throw invalid_argument("Must be number.");
 		}
+
+		if (isDuplicatedNumber(guessNumber)) {
+			throw invalid_argument("Must not have the same number.");
+		}
+	}
+
+	void guess(const string guessNumber)
+	{
+		assertIllegalArgument(guessNumber);
 	}
 };
